5ef6248b7d2f6ad0e89b96964127e6c83b60297a
Gustavo da Silva Rosa <gustavodasrosa@gmail.com>
2024-04-04 00:42:09 -0300

    finalized project

--- src/app/ticket.tsx
+++ src/app/ticket.tsx
@@ -7,9 +7,14 @@ import {
    TouchableOpacity,
    Alert,
    Modal,
+   Share,
 } from "react-native";
+import { MotiView } from "moti";
 import { FontAwesome } from "@expo/vector-icons";
 import * as ImagePicker from "expo-image-picker";
+import { Redirect } from "expo-router";
+
+import { useBadgeStore } from "@/store/badge-store";
 
 import { Header } from "@/components/header";
 import { Credential } from "@/components/credential";
@@ -18,8 +23,22 @@ import { colors } from "@/styles/colors";
 import { QRCode } from "@/components/qrcode";
 
 export default function Ticket() {
-   const [image, setImage] = useState("");
    const [expandQRCode, setExpandQRCode] = useState<boolean>(false);
+
+   const badgeStore = useBadgeStore();
+
+   async function handleShare() {
+      try {
+         if (badgeStore.data?.checkInURL) {
+            await Share.share({
+               message: badgeStore.data.checkInURL,
+            });
+         }
+      } catch (error) {
+         console.log(error);
+         Alert.alert("Compartilhar", "Não foi possível compartilhar.");
+      }
+   }
 
    async function handleSelectImage() {
       try {
@@ -30,12 +49,16 @@ export default function Ticket() {
          });
 
          if (result.assets) {
-            setImage(result.assets[0].uri);
+            badgeStore.updateAvatar(result.assets[0].uri);
          }
       } catch (error) {
          console.log(error);
          Alert.alert("Foto", "Não foi possível selecionar a imagem.");
       }
+   }
+
+   if (!badgeStore.data?.checkInURL) {
+      return <Redirect href="/" />;
    }
 
    return (
@@ -50,29 +73,44 @@ export default function Ticket() {
             showsVerticalScrollIndicator={false}
          >
             <Credential
-               image={image}
+               data={badgeStore.data}
                onChangeAvatar={handleSelectImage}
                onExpandQRCode={() => setExpandQRCode(true)}
             />
 
-            <FontAwesome
-               name="angle-double-down"
-               size={24}
-               color={colors.gray[300]}
-               className="self-center my-6"
-            />
+            <MotiView
+               from={{ translateY: 0 }}
+               animate={{ translateY: 10 }}
+               transition={{
+                  loop: true,
+                  type: "timing",
+                  duration: 700,
+               }}
+            >
+               <FontAwesome
+                  name="angle-double-down"
+                  size={24}
+                  color={colors.gray[300]}
+                  className="self-center my-6"
+               />
+            </MotiView>
 
             <Text className="text-white font-bold text-2xl mt-4">
                Compartilhar credencial
             </Text>
 
             <Text className="text-white font-regular text-base mt-1 mb-6">
-               Mostre ao mundo que você vai participar do Unite Summit!
+               Mostre ao mundo que você vai participar do evento{" "}
+               {badgeStore.data.eventTitle}!
             </Text>
 
-            <Button title="Compartilhar" />
+            <Button title="Compartilhar" onPress={handleShare} />
 
-            <TouchableOpacity activeOpacity={0.7} className="mt-10">
+            <TouchableOpacity
+               activeOpacity={0.7}
+               className="mt-10"
+               onPress={() => badgeStore.remove()}
+            >
                <Text className="text-base text-white font-bold text-center">
                   Remover Ingresso
                </Text>
--- src/components/credential.tsx
+++ src/components/credential.tsx
@@ -3,21 +3,53 @@ import {
    View,
    ImageBackground,
    Text,
+   useWindowDimensions,
    TouchableOpacity,
 } from "react-native";
 import { Feather } from "@expo/vector-icons";
+import { MotiView } from "moti";
+
 import { colors } from "@/styles/colors";
+
 import { QRCode } from "@/components/qrcode";
 
+import { BadgeStore } from "@/store/badge-store";
+
 type Props = {
-   image?: string;
+   data: BadgeStore;
    onChangeAvatar?: () => void;
    onExpandQRCode?: () => void;
 };
 
-export function Credential({ onChangeAvatar, onExpandQRCode, image }: Props) {
+export function Credential({ data, onChangeAvatar, onExpandQRCode }: Props) {
+   const { height } = useWindowDimensions();
+
    return (
-      <View className="w-full self-stretch items-center">
+      <MotiView
+         className="w-full self-stretch items-center"
+         from={{
+            opacity: 0,
+            translateY: -height,
+            rotateZ: "50deg",
+            rotateY: "30deg",
+            rotateX: "30deg",
+         }}
+         animate={{
+            opacity: 1,
+            translateY: 0,
+            rotateZ: "0deg",
+            rotateY: "0deg",
+            rotateX: "0deg",
+         }}
+         transition={{
+            type: "spring",
+            damping: 20,
+            rotateZ: {
+               damping: 15,
+               mass: 3,
+            },
+         }}
+      >
          <Image
             source={require("@/assets/ticket/band.png")}
             className="w-24 h-52 z-10"
@@ -30,18 +62,20 @@ export function Credential({ onChangeAvatar, onExpandQRCode, image }: Props) {
             >
                <View className="w-full flex-row items-center justify-between">
                   <Text className="text-zinc-50 text-sm font-bold">
-                     Unite summit
+                     {data.eventTitle}
                   </Text>
-                  <Text className="text-zinc-50 text-sm font-bold">#123</Text>
+                  <Text className="text-zinc-50 text-sm font-bold">
+                     #{data.id}
+                  </Text>
                </View>
 
-               <View className="w-40 h-40 bg-black rounded-full" />
+               {/* <View className="w-40 h-40 bg-black rounded-full" /> */}
             </ImageBackground>
 
-            {image ? (
+            {data.image ? (
                <TouchableOpacity activeOpacity={0.9} onPress={onChangeAvatar}>
                   <Image
-                     source={{ uri: image }}
+                     source={{ uri: data.image }}
                      className="w-36 h-36 rounded-full -mt-24"
                   />
                </TouchableOpacity>
@@ -56,14 +90,14 @@ export function Credential({ onChangeAvatar, onExpandQRCode, image }: Props) {
             )}
 
             <Text className="font-bold text-2xl text-zinc-50 mt-4">
-               Gustavo Rosa
+               {data.name}
             </Text>
 
             <Text className="font-regular text-base text-zinc-300 mb-4">
-               gustavodasrosa@gmail.com
+               {data.email}
             </Text>
 
-            <QRCode value="teste" size={120} />
+            <QRCode value={data.checkInURL} size={120} />
 
             <TouchableOpacity
                activeOpacity={0.7}
@@ -75,6 +109,6 @@ export function Credential({ onChangeAvatar, onExpandQRCode, image }: Props) {
                </Text>
             </TouchableOpacity>
          </View>
-      </View>
+      </MotiView>
    );
 }
